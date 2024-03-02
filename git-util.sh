b1a6367bba9b3cb27338cc479933e993e2ca3ff3
Matt Kirkley <matt.kirkley@gmail.com>
2024-03-02 17:50:45 -0500

    Is branch dirty and current branch functions

--- git-util.sh
+++ git-util.sh
@@ -1,1 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+__current_branch() {
+  echo $(git --no-optional-locks symbolic-ref --short HEAD 2>/dev/null || git --no-optional-locks rev-parse --short HEAD 2>/dev/null);
+}
+
+__is_branch_dirty() {
+  if [[ $(git --no-optional-locks ls-files --error-unmatch -m --directory --no-empty-directory -o --exclude-standard ":/*" > /dev/null 2>&1;) != '' ]]; then
+    echo 'true'
+  else
+    echo ''
+  fi
+}
+
