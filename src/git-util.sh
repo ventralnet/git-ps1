aede1d98fc670e8366aeef37dbbea5ea1dcc70c2
Matt Kirkley <matt.kirkley@gmail.com>
2024-03-02 19:19:48 -0500

    __is_dirty __is_staged and __current_branch utility methods and tests

---
+++ src/git-util.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+__current_branch() {
+  echo $(git --no-optional-locks symbolic-ref --short HEAD 2>/dev/null || git --no-optional-locks rev-parse --short HEAD 2>/dev/null);
+}
+
+__has_staged() {
+  if git status --porcelain | grep -qE "^[[:space:]](A|M|D)"; then
+    echo "true"
+  else
+    echo ""
+  fi
+}
+
+__is_dirty() {
+  # Check for unstaged changes with --shortstat option
+  if git diff --shortstat 2> /dev/null | grep -qE "[[:digit:]]+ file[s]* changed"; then
+    echo "true"
+  # Check for untracked files with --porcelain option
+  elif git status --porcelain 2> /dev/null | grep -qE "^??"; then
+    echo "true"
+  # Check for new files with --cached option (implicitly staged)
+  elif git diff --cached --name-only 2> /dev/null | grep -qE "."; then
+    echo "true"
+  else
+    echo ""
+  fi
+}
