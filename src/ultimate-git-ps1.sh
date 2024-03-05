89d47d6d3eb15a42019a77c5fcaa64eca19cea64
Matt Kirkley <matt.kirkley@gmail.com>
2024-03-04 19:57:57 -0500

    Starter ps1

--- src/ultimate-git-ps1.sh
+++ src/ultimate-git-ps1.sh
@@ -1,1 +1,7 @@
 #!/bin/sh
+
+DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+
+. $DIR/git-util.sh
+
+export PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w$(__git_ps1)\[\033[00m\]\$ "
